969df6e60b209e6581514f33d3561492a54cfb6c
Ingolf Ankert <github@ingank.net>
2021-01-08 14:47:25 +0100

    Slash Kommando implementieren

--- WoW-Addon.lua
+++ WoW-Addon.lua
@@ -2,5 +2,19 @@ local addonName, addon = ...
 
 addon.version = GetAddOnMetadata(addonName, "Version")
 addon.notes = GetAddOnMetadata(addonName, "Notes")
+SLASH_WOWADDON1, SLASH_WOWADDON2 = "/wowaddon", "/wowa"
 
-print(addonName .. " // " .. addon.notes .. " // Version " .. addon.version)
+function addon.Init()
+    print(addonName .. " // " .. addon.notes .. " // Version " .. addon.version)
+end
+
+function SlashCmdList.WOWADDON(msg, editBox)
+    local command, rest = msg:match("^(%S*)%s*(.-)$")
+    if command ~= "" and rest ~= "" then
+        print("Kommando: " .. command .. " // Argumente: " .. rest)
+    else
+        print("Syntax: /wowa Kommando Argumente")
+    end
+end
+
+addon.Init()
